71f03f17f67263ad804ee7892c17dba9c3d65f43
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2019-09-26 14:22:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 let recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value){
-
    let newObj = Object.assign({},object);
    newObj[key] = value;
    return newObj;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-
-
    object[key] = value;
    return object;
 }
